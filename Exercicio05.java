07a43a6204685b1bf48059724761e841ef8473ab
V工匚匕口尺ᵃʸ <89154798+VictorGomes06@users.noreply.github.com>
2023-07-27 21:27:17 -0300

    Update Exercicio05.java

--- Exercicio05.java
+++ Exercicio05.java
@@ -4,13 +4,13 @@ public class Exercicio05 {
 
 	public static void main(String[] args) {
 		int codigoPeca01 = 10;
-		int numDepecas01 = 1;
-		double valorDaPeca01 = 5.30;
+		int numDepecas01 = 0;
+		double valorDaPeca01 = 0.0;
 		double valorToalPeca01 = valorDaPeca01 * numDepecas01;
 		
-		int codigoPeca02 = 10;
-		int numDepecas02 = 2;
-		double valorDaPeca02 = 5.10;
+		int codigoPeca02 = 12;
+		int numDepecas02 = 0;
+		double valorDaPeca02 = 0.0;
 		double valorToalPeca02 = valorDaPeca02 * numDepecas02;
 		
 		double total = valorToalPeca01 + valorToalPeca02;
