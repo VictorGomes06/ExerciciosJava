3c5a58d6c6be7ee420065ebefb63f473a8c5dadc
V工匚匕口尺ᵃʸ <89154798+VictorGomes06@users.noreply.github.com>
2023-07-27 21:27:45 -0300

    Update Exercicio3.java

--- Exercicio3.java
+++ Exercicio3.java
@@ -2,10 +2,10 @@ package Exercicios;
 
 public class Exercicio3 {
 	public static void main(String[] args) {
-		int a = 5;
-		int b = 6;
-		int c = 7;
-		int d = 8;
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		int d = 0;
 		
 		int diferenca = a*b-c*d; 
 		
